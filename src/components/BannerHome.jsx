c6822c638f5c08e0d97e18d846fe552e374a3cc2
Omer Faruk Ozkan <faruk_s.v.i@hotmail.com>
2025-02-05 02:29:25 +0300

    add the play now link in the banner

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const BannerHome = () => {
   const { bannerData, imageURL } = useSelector((state) => state.movieoData);
@@ -80,9 +81,12 @@ const BannerHome = () => {
                   <span>|</span>
                   <p>View: {Number(data.popularity).toFixed(0)}</p>
                 </div>
-                <button className="bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105 cursor-pointer">
+                <Link
+                  to={`/${data?.media_type}/${data?.id}`}
+                  className="bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105 cursor-pointer"
+                >
                   Play Now
-                </button>
+                </Link>
               </div>
             </div>
           </div>
